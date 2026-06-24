b09300b740b7d8142bec5543d4f0010a453ba98b
luiz arcanjo <134141921+luizarcanjo@users.noreply.github.com>
2026-06-24 18:05:02 +0000

    ajustando users front

--- front/routes/users.js
+++ front/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const url = "https://fluffy-tribble-vx7r9vp5v7j2579-4000.app.github.dev/users"
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  fetch(url, { method: 'GET' })
+    .then(async (res) => {
+      if(!res.ok){
+        const err = await res.json()
+        throw err
+      }
+      return res.json()
+    })
+    .then((users) => {
+      let title = "Gestão de usuários"
+      let cols=["Nome","Senha","Email","Telefone","Acões"]
+      res.render('users', { title ,users, cols, error: ""})
+    })
+    .catch((error) => {
+      console.log('erro',error)
+      res.status(500).send("Erro ao buscar usuários", error)
+    })
+  
 });
 
 module.exports = router;
